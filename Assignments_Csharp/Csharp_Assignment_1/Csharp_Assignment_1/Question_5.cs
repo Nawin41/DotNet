5800f609333bc8f333fb07b2713ac353ad41dec6
Nawin Sharvesh <nawins@infinite.com>
2026-03-25 17:33:07 +0530

    Question 5 added

---
+++ Assignments_Csharp/Csharp_Assignment_1/Csharp_Assignment_1/Question_5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Csharp_Assignment_1
+{
+    internal class Question_5
+    {
+        //5.  Write a C# program to compute the sum of two given integers. If two values are the same, return the triple of their sum.
+        public static void Run()
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Triple sum = " + (sum * 3));
+            }
+            else
+            {
+                Console.WriteLine("Sum = " + sum);
+            }
+        }
+    }
+}
