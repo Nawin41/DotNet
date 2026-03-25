36a6a78a18a04c981e28c7ef2b32a857924cc9a3
Nawin Sharvesh <nawins@infinite.com>
2026-03-25 17:32:13 +0530

    Question 4 added

---
+++ Assignments_Csharp/Csharp_Assignment_1/Csharp_Assignment_1/Question_4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Csharp_Assignment_1
+{
+    //4. Write a C# Sharp program that prints the multiplication table of a number as input.
+    internal class Question_4
+    {
+        public static void Run()
+        {
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int i;
+
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(num + " * " + i + " = " + (num * i));
+            }
+        }
+    }
+}
