7189eab1501d489301d542f293fc2508413cf8f3
Nawin Sharvesh <nawins@infinite.com>
2026-03-25 17:12:35 +0530

    Program file added

---
+++ Assignments_Csharp/Csharp_Assignment_1/Csharp_Assignment_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question_1.Run();
+            Question_2.Run();
+            Question_3.Run();
+            Question_4.Run();
+            Question_5.Run();  
+            Console.ReadLine();
+        }
+    }
+}
+
