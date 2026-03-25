5f003c0a0263954f63042215b7dfead073a5e17f
Nawin Sharvesh <nawins@infinite.com>
2026-03-25 17:28:53 +0530

    Question 1 added

---
+++ Assignments_Csharp/Csharp_Assignment_1/Csharp_Assignment_1/Question_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Csharp_Assignment_1
+{
+    // Question 1: Write a C# program to check if two numbers are equal or not.
+    internal class Question_1
+    {
+        public static void Run()
+        {
+            Console.Write("Input 1st number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Input 2nd number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine(num1 + " and " + num2 + " are equal");
+            }
+            else
+            {
+                Console.WriteLine(num1 + " and " + num2 + " are not equal");
+            }
+        }
+    }
+}
