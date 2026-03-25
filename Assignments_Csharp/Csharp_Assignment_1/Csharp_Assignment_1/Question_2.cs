ea7945a7889398dd6d0d493a05b83f2ee9950a44
Nawin Sharvesh <nawins@infinite.com>
2026-03-25 17:31:34 +0530

    Question 2 added

---
+++ Assignments_Csharp/Csharp_Assignment_1/Csharp_Assignment_1/Question_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Csharp_Assignment_1
+{
+    // Question 2: Write a C# program to check if a number is positive or negative.
+   
+    internal class Question_2
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Question 2");
+
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                Console.WriteLine(num + " is a positive number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is a negative number");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
