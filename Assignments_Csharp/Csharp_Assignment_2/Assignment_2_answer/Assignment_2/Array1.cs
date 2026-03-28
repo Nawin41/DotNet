f0de729712e33ee878ebfbbc19c564ddb829e14c
Nawin Sharvesh <nawins@infinite.com>
2026-03-28 11:03:32 +0530

    Assignment 2 Array Question 1 added

---
+++ Assignments_Csharp/Csharp_Assignment_2/Assignment_2_answer/Assignment_2/Array1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    internal class Array1
+    {
+        public static void Run()
+        {
+            int[] arr = new int[5];  
+
+            Console.WriteLine("Enter 5 numbers:");
+
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            int min = arr[0];
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+
+                if (arr[i] < min)
+                    min = arr[i];
+
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            double avg = (double)sum / arr.Length;
+
+            Console.WriteLine("Average = " + avg);
+            Console.WriteLine("Minimum = " + min);
+            Console.WriteLine("Maximum = " + max);
+        }
+    }
+}
