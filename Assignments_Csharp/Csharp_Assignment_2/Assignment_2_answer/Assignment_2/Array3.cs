d661cec4a2a1488f47d9289d5c19969f72326b67
Nawin Sharvesh <nawins@infinite.com>
2026-03-28 11:04:12 +0530

    Assignment 2 Array Question 3 added

---
+++ Assignments_Csharp/Csharp_Assignment_2/Assignment_2_answer/Assignment_2/Array3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    //Copying elements from one array to another array
+    internal class Array3
+    {
+        public static void Run()
+        {
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+
+            Console.WriteLine("Enter 5 elements for first array:");
+
+            //Input for first array
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.Write("Enter element " + (i + 1) + ": ");
+                arr1[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Copy
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+
+            Console.WriteLine("Elements of second array (copied):");
+
+            //second array
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+        }
+    }
+}
