86636e0105bf3d11491a0695d475620edc597ae4
Nawin Sharvesh <nawins@infinite.com>
2026-03-28 10:59:46 +0530

    Assignment 2 Program file added

---
+++ Assignments_Csharp/Csharp_Assignment_2/Assignment_2_answer/Assignment_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A2Q1.Run();
+            Console.WriteLine();
+
+            A2Q2.Run();
+            Console.WriteLine();
+
+            Array1.Run();
+            Console.WriteLine();
+
+            Array2.Run();
+            Console.WriteLine();
+
+            Array3.Run();
+            Console.WriteLine();
+
+            String_questions.Run();
+        }
+    }
+}
