ce858adc874996d3cbd9779f5aeeb63204359322
Nawin Sharvesh <nawins@infinite.com>
2026-04-02 11:53:50 +0530

    Code Challenge 1 Added

---
+++ Code_Challenges/C_Sharp_Code_Challenges/C_Sharp_Code_Challenge_1/Employee_Structure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Code_Challenge_1
+{
+    class Employee_Structure
+    {
+        // Nested structure
+        struct BirthDate
+        {
+            public int Day;
+            public int Month;
+            public int Year;
+        }
+
+        struct Employee
+        {
+            public string Name;
+            public BirthDate DOB;
+        }
+
+        public void Run()
+        {
+            Employee[] emp = new Employee[2];
+
+            Console.WriteLine("----Create a nested structure and store data in an array :----");
+
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.Write("Name of the employee : ");
+                emp[i].Name = Console.ReadLine();
+
+                Console.Write("Input day of the birth : ");
+                emp[i].DOB.Day = int.Parse(Console.ReadLine());
+
+                Console.Write("Input month of the birth : ");
+                emp[i].DOB.Month = int.Parse(Console.ReadLine());
+
+                Console.Write("Input year for the birth : ");
+                emp[i].DOB.Year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----Employee Details:----");
+
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine("Name : " + emp[i].Name);
+                Console.WriteLine("Date of Birth : " + emp[i].DOB.Day + "/" + emp[i].DOB.Month + "/" + emp[i].DOB.Year);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Code_Challenges/C_Sharp_Code_Challenges/C_Sharp_Code_Challenge_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_Sharp_Code_Challenge_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code challenge 1 Question 1 - Employee Management System
+            Emp_management_system emp_system = new Emp_management_system();
+            emp_system.Run();
+
+            //Code challenge 1 Question 2 - Employee Structure
+
+            Employee_Structure emp_str = new Employee_Structure();
+            emp_str.Run();
+        }
+    }
+}
