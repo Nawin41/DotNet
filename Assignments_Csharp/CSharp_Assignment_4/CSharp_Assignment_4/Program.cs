a63b572c4af4ec00bb8fa15704938375db932fcf
Nawin Sharvesh <nawins@infinite.com>
2026-04-02 22:03:19 +0530

    Assignment 4 Program.cs

---
+++ Assignments_Csharp/CSharp_Assignment_4/CSharp_Assignment_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1 
+           // StringOperations obj = new StringOperations();
+            //obj.RemoveCharacter();
+
+
+            //Question 2
+            //String_Exchange exchange = new String_Exchange();
+            //exchange.SwapFirstLast();
+
+            //Question 3
+            Descending_Order descending = new Descending_Order();
+            descending.SortStackDescending();
+        }
+    }
+}
