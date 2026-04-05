68ab998c73ded12a8d65ad11b918ba9f33ec929e
Nawin Sharvesh <nawins@infinite.com>
2026-04-05 09:54:32 +0530

    Program file Addded

---
+++ Assignments_Csharp/CSharp_Assignment_5/CSharp_Assignment_5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp_Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankApplication.Start();
+            Scholarship.ApplyScholarship();
+            BookShelfOperations.Start();
+        }
+    }
+}
