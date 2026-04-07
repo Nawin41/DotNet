ef20e78e1f9cec26987a29c28bd7dd6ddfa927f3
Nawin Sharvesh <nawins@infinite.com>
2026-04-07 09:29:10 +0530

    A6Q2 added

---
+++ Assignments_Csharp/CSharp_Assignment_6/CSharp_Assignment_6/file_creation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+//Question 2
+
+namespace CSharp_Assignment_6
+{
+    class file_creation
+    {
+        public static void Execute()
+        {
+            string filePath = "C:\\Training_DotNet\\Assignments_Csharp\\CSharp_Assignment_6\\MyTextFile.txt";
+
+            
+            string[] lines =
+            {
+                "Hello",
+                "World",
+                "Let's",
+                "Learn",
+                "Dot Net"
+            };
+
+            
+            File.WriteAllLines(filePath, lines);
+            Console.WriteLine("Data written to file successfully.\n");
+
+            
+            Console.WriteLine("Reading data from file:\n");
+            string[] readLines = File.ReadAllLines(filePath);
+
+            foreach (string line in readLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
