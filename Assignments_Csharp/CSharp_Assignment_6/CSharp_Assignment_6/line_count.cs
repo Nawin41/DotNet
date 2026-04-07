36f168414bb18890b05d401872b68cc836d19d7f
Nawin Sharvesh <nawins@infinite.com>
2026-04-07 09:30:22 +0530

    A6Q3 added

---
+++ Assignments_Csharp/CSharp_Assignment_6/CSharp_Assignment_6/line_count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+//Question 3
+namespace CSharp_Assignment_6
+{
+    class line_count
+    {
+        public static void Execute()
+        {
+            string filePath = "C:\\Training_DotNet\\Assignments_Csharp\\CSharp_Assignment_6\\MyTextFile.txt";
+
+            
+            if (File.Exists(filePath))
+            {
+                
+                string[] lines = File.ReadAllLines(filePath);
+
+                //Count number of lines
+                int lineCount = lines.Length;
+
+                Console.WriteLine($"Total number of lines in the file: {lineCount}");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+        }
+    }
+}
