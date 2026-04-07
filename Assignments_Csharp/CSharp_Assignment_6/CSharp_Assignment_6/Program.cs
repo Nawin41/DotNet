cb16296c73e87b3917e535f04465c56c14a1bf96
Nawin Sharvesh <nawins@infinite.com>
2026-04-07 09:25:46 +0530

    A6 program file added

---
+++ Assignments_Csharp/CSharp_Assignment_6/CSharp_Assignment_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1
+            BookQuestion.Execute();
+            //Question 2
+            file_creation.Execute();
+            //Question 3
+            line_count.Execute();
+
+            Console.ReadLine();
+
+        }
+    }
+}
